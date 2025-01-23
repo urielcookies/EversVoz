5cf5e794b9bcc37cdf890fa70b5d940b9e950db6
Ever Uriel Garcia <urielcookies@outlook.com>
2025-01-22 22:54:11 -0700

    Integrate Adapty for enhanced user engagement; activate with public key in App.tsx

--- App.tsx
+++ App.tsx
@@ -1,4 +1,7 @@
 import Root from './src/Root';
+import { adapty } from 'react-native-adapty';
+
+adapty.activate(process.env.EXPO_PUBLIC_ADAPTY_KEY as string);
 
 export default function App() {
   return <Root />;
