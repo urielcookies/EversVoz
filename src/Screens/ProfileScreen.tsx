2783619ff4f6113528f9c7f78776a493b1910837
Ever Uriel Garcia <urielcookies@outlook.com>
2025-01-26 12:31:29 -0700

    Integrate subscription details display in ProfileScreen; fetch and format expiration date and renewal status

--- src/Screens/ProfileScreen.tsx
+++ src/Screens/ProfileScreen.tsx
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, StyleSheet, Alert } from 'react-native';
+import { adapty } from 'react-native-adapty';
+import { format, parseISO } from 'date-fns';
+import { es } from 'date-fns/locale';
 import { supabaseAdmin } from '../Utils/supabase';
 import ButtonElement from '../Components/ButtonElement';
 import SwitchElement from '../Components/SwitchElement';
@@ -7,11 +10,40 @@ import { useDarkMode } from '../Contexts/DarkModeContext';
 import CardElement from '../Components/CardElement';
 import TextElement from '../Components/TextElement';
 import { useUserSession } from '../Contexts/UserSessionContext';
-import { isNull } from 'lodash';
+import { isEmpty } from 'lodash';
+
 
 const ProfileScreen = () => {
   const { isDarkMode, toggleDarkMode } = useDarkMode();
   const { logout, deleteUserAccount } = useUserSession();
+  const [ subscription, setSubscription] = useState({
+    expirationDate: '',
+    renews: 'No',
+  });
+
+  useEffect(() => {
+    const fetchProfile = async () => {
+      try {
+        const profile = await adapty.getProfile();
+        console.log('basic_tier-->>', profile.accessLevels?.basic_tier)
+
+        const expirationDate = profile.accessLevels?.basic_tier?.expiresAt
+          ? format(profile.accessLevels?.basic_tier.expiresAt, "d 'de' MMMM 'de' yyyy 'a las' hh:mm:ss a", { locale: es })
+          : profile.accessLevels?.basic_tier?.unsubscribedAt
+            ? format(profile.accessLevels?.basic_tier.unsubscribedAt, "d 'de' MMMM 'de' yyyy 'a las' hh:mm:ss a", { locale: es })
+            : ''
+
+        setSubscription({
+          expirationDate,
+          renews: profile.accessLevels?.basic_tier.willRenew ? 'Si' : 'No'
+        });
+      } catch (error) {
+        console.error('Error fetching profile:', error);
+      }
+    };
+
+    fetchProfile();
+  }, []);
 
   const handleDeleteAccount = () => {
     Alert.alert(
@@ -29,7 +61,7 @@ const ProfileScreen = () => {
     <View style={[styles.container, { backgroundColor: isDarkMode ? '#121212' : '#f5f5f5' }]}>
       <CardElement>
         <View style={styles.row}>
-          <TextElement style={styles.text}>Modo Oscuro</TextElement>
+          <TextElement bold style={styles.text}>Modo Oscuro</TextElement>
           <SwitchElement value={isDarkMode} onValueChange={toggleDarkMode} />
         </View>
 
@@ -48,6 +80,26 @@ const ProfileScreen = () => {
           width="100%"
           icon="trash-o" />
       </CardElement>
+
+    {!isEmpty(subscription) && (
+      <>
+        <CardElement>
+          <View style={styles.subscription}>
+            <TextElement bold style={styles.text}>Fin De Suscripción</TextElement>
+            <View style={styles.spacer} />
+            <TextElement style={styles.text}>{subscription.expirationDate}</TextElement>
+          </View>
+        </CardElement>
+
+        <CardElement>
+          <View style={styles.subscription}>
+            <TextElement bold style={styles.text}>Renueva Suscripción</TextElement>
+            <View style={styles.spacer} />
+            <TextElement style={styles.text}>{String(subscription.renews)}</TextElement>
+          </View>
+        </CardElement>
+      </>
+    )}
     </View>
   );
 };
@@ -68,11 +120,15 @@ const styles = StyleSheet.create({
   },
   text: {
     fontSize: 18,
-    fontWeight: '600',
   },
   spacer: {
     height: 20,
   },
+  subscription: {
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    textAlign: 'center'
+  }
 });
 
 export default ProfileScreen;
